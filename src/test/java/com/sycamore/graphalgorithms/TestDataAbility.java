b4bec9af88b83d3d1b982b525e4e6ba16445b89a
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2025-01-12 16:37:28 +0100

    initial

---
+++ src/test/java/com/sycamore/graphalgorithms/TestDataAbility.java
@@ -0,0 +1,8 @@
+package com.sycamore.graphalgorithms;
+
+public interface TestDataAbility {
+    String BERLIN = "Berlin";
+    String PARIS = "Paris";
+    String PRAGUE = "Prague";
+    String WARSAW = "Warszawa";
+}
